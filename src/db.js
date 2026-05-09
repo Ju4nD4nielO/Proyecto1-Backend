5f7e8822d264cffb9290b17000e25cb7f8b665de
Juan Daniel Ordoñez <ord24979@uvg.edu.gt>
2026-05-08 18:21:17 -0600

    Cambios en Docker

--- src/db.js
+++ src/db.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: false
-  // ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
-module.exports = pool;    
+module.exports = pool;
